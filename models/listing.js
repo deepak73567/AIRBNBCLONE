1a287dd58893823f0334724faaf76947c74de4be
Deepak Vishwakarma <114127594+deepak73567@users.noreply.github.com>
2025-02-23 10:49:13 +0530

    Update listing.js

--- models/listing.js
+++ models/listing.js
@@ -30,11 +30,11 @@ const listingSchema=new Schema({
     type: {
       type: String,
       enum: ['Point'], // Corrected enum value
-      required: true
+      
     },
     coordinates: {
       type: [Number],
-      required: true
+      
     },
   },
 });
